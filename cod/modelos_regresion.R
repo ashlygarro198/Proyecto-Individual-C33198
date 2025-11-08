97e389eb00cb526c4e533b12ce8143056be90c27
Ashly Garro <garroashly7@gmail.com>
2025-11-08 14:01:48 -0600

    fix: cambio en modelos_regresion.R

--- cod/modelos_regresion.R
+++ cod/modelos_regresion.R
@@ -13,7 +13,7 @@ modelo_roi <- lm(ROI_log ~ log(budget) + vote_average + runtime, data = movies)
 print(summary(modelo_roi))
 
 ## Modelo 2: Ingresos como indicador de exito
-modelo_revenue <- lm(revenue ~ log(budget) + vote_average, data = movies)
+modelo_revenue <- lm(log(revenue) ~ log(budget) + vote_average, data = movies)
 print(summary(modelo_revenue))
 
 ## Modelo 3: Promedio de calificaciones como indicador de exito
